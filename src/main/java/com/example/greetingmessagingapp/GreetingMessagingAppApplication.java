2b9ecdbc45d941fde9d9d2db1ced1c5bf4c08282
shravkv <sravan.k.vodnala@gmail.com>
2022-09-02 21:19:19 +0530

    Added: added greeting messaging app

---
+++ src/main/java/com/example/greetingmessagingapp/GreetingMessagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingmessagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingMessagingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingMessagingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greetingmessagingapp/GreetingMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingmessagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingMessagingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
