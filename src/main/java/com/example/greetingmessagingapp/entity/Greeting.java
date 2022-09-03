150ee290507349cd6c39ffde64274e3aa424d911
shravkv <sravan.k.vodnala@gmail.com>
2022-09-03 09:00:46 +0530

    Added: save greeting message to repository

--- src/main/java/com/example/greetingmessagingapp/entity/Greeting.java
+++ src/main/java/com/example/greetingmessagingapp/entity/Greeting.java
@@ -1,21 +1,48 @@
-package com.example.greetingmessagingapp.entity;
+package com.bridgelabz.greetingapp.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
 public class Greeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", nullable = false)
 
     private long id;
-    private String message;
+    private String firstName;
+    private String lastName;
+    private String message = "Hello" + firstName + " " + lastName;
+    public Greeting() {
 
-    public Greeting(long id, String message) {
-        this.id = id;
-        this.message = message;
     }
 
-    public long getId() {
-        return id;
+    public Greeting(long id, String firstName, String lastName, String message) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.message = message;
     }
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getMessage() {
@@ -26,4 +53,11 @@ public class Greeting {
         this.message = message;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
